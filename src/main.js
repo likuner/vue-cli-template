d9035d960c104f051f25dbb20f100c7a7b882b1e
Hunter <273676636@qq.com>
2022-07-07 10:52:17 +0800

    add view-ui-plus

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
+import ViewUIPlus from 'view-ui-plus'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
 // import './mock'
+import 'view-ui-plus/dist/styles/viewuiplus.css'
 
 const app = createApp(App)
 
 app.use(store)
+  .use(ViewUIPlus)
   .use(router)
   .mount('#app')
